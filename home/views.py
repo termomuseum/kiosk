ca48ffce87e2e0252d62c90cfdf7698154b0b811
RostyslavBryiovskyi <rostyslav.bryiovskyi@gmail.com>
2019-04-16 12:53:03 +0200

    Add

--- home/views.py
+++ home/views.py
@@ -6,5 +6,18 @@ def index(request):
   content = 'Hello, Kiosk!'
   return HttpResponse(content=content, content_type='text/plain')
 
+
+def view_image(request):
+  pass
+
+
+def view_video(request):
+  pass
+
+
+def view_presentation(request):
+  pass
+
+
 def temp(request):
 	return render(request, 'home/home.html')
