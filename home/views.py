1f3c56e4af94da0ab22f4ea0f8e058d11dd266fb
RostyslavBryiovskyi <rostyslav.bryiovskyi@gmail.com>
2019-04-16 13:13:35 +0200

    add

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ def index(request):
   return HttpResponse(content=content, content_type='text/plain')
 
 def temp(request):
-	obj = GalleryEntry.objects.get(entry_name='Poroshenko')
+	obj = GalleryEntry.objects.get(entry_name='Bug')
 	video = obj.entry_file_url
 	print(video)
 	return render(request, 'home/home.html', {'obj':video})
