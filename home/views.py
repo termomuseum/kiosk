74e5d03ef5ed79fba672640d0c41fa9ac31acc6b
Vitalii Brydinskyi <fartenko@gmail.com>
2019-04-16 12:50:08 +0200

    Update Views

--- home/views.py
+++ home/views.py
@@ -5,3 +5,16 @@ from django.http import HttpResponse
 def index(request):
   content = 'Hello, Kiosk!'
   return HttpResponse(content=content, content_type='text/plain')
+
+
+def view_image(request):
+  pass
+
+
+def view_video(request):
+  pass
+
+
+def view_presentation(request):
+  pass
+
