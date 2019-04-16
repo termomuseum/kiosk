8df70ac82194705d2e695db5f0bc0825c5b83052
Vitalii Brydinskyi <fartenko@gmail.com>
2019-04-16 13:11:40 +0200

    Commit Accepted Change

--- home/views.py
+++ home/views.py
@@ -3,24 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-  content = 'Hello, Kiosk! :)'
+  content = 'Hello, Kiosk!'
   return HttpResponse(content=content, content_type='text/plain')
-
-
-def view_image(request):
-  # return ...
-  pass
-
-
-def view_video(request):
-  # return ...
-  pass
-
-
-def view_presentation(request):
-  # return ...
-  pass
-
 
 def temp(request):
 	return render(request, 'home/home.html')
