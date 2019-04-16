4e142683886cb47a76ea49bebfa22ecf4b38cd2a
RostyslavBryiovskyi <rostyslav.bryiovskyi@gmail.com>
2019-04-16 13:08:08 +0200

    add

--- home/views.py
+++ home/views.py
@@ -1,23 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import GalleryEntry
 
 
 def index(request):
   content = 'Hello, Kiosk!'
   return HttpResponse(content=content, content_type='text/plain')
 
-
-def view_image(request):
-  pass
-
-
-def view_video(request):
-  pass
-
-
-def view_presentation(request):
-  pass
-
-
 def temp(request):
-	return render(request, 'home/home.html')
+	obj = GalleryEntry.objects.get(entry_name='Poroshenko')
+	video = obj.entry_file_url
+	print(video)
+	return render(request, 'home/home.html', {'obj':video})
