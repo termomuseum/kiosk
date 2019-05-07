5066cd389c2c59542b46c3382b467de114870b26
Vitalii Brydinskyi <fartenko@gmail.com>
2019-05-07 11:21:20 +0200

    Add category field to GalleryEntry model

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from home.models import GalleryEntry, GalleryEntryType
+from home.models import GalleryEntry, GalleryEntryType, GalleryEntryCategory
 
 # Register your models here.
+admin.site.register(GalleryEntryCategory)
 admin.site.register(GalleryEntryType)
 admin.site.register(GalleryEntry)
