4dda02ebbb613c41bf6788f56fffaab84557c76d
Vitalii Brydinskyi <fartenko@gmail.com>
2019-05-21 13:31:08 +0200

    Add drag scroll with dragscroll.js and add blocks.

---
+++ home/static/home/js/main.js
@@ -0,0 +1,35 @@
+//import ScrollBooster from 'scrollbooster'
+/*
+let viewport = document.querySelector('.viewport')
+let content = document.querySelector('.viewport-content')
+
+// ScrollBooster
+let sb = new ScrollBooster({
+  viewport: viewport,
+  content: content,
+  mode: 'y',
+  handle: document.querySelector('.viewport-scroller'),
+  bounce: true,
+  textSelection: false,
+  emulateScroll: false,
+
+  onUpdate: (data) => {
+    content.style.transform = `translateY(${-data.position.y}px)`
+  },
+
+  
+  //shouldScroll: (data, event) => {
+  //  if (event.target.classList.contains('button')) {
+  //    return false
+  //  } else {
+  //    return true
+  //  }
+  //},
+  //
+  //onClick: (data, event) => {
+  //  if (event.target.classList.contains('link')) {
+  //    event.preventDefault()
+  //  }
+  //}
+  
+})*/
