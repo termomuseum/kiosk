7504066c9d8d585aa912ac1250c326f47a86054e
Vitalii Brydinskyi <fartenko@gmail.com>
2019-06-16 19:02:44 +0200

    Update editor.

--- home/views.py
+++ home/views.py
@@ -155,46 +155,61 @@ def editor(request):
 
   # Adding new category
   if editor_type == "add_cat":
-    category_name = request.POST.get("cname", "None")
-    editor_add_new_category(category_name)
+    try:
+      # getting category name and create it
+      category_name = request.POST.get("cname", "None")
+      editor_add_new_category(category_name)
 
-    show_success = True
-    success_msg = "Category '{}' added successfuly. \n".format(category_name)
+      show_success = True
+      success_msg = "Category '{}' added successfuly. \n".format(category_name)
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
+
 
   # Adding new entry
   if editor_type == "add_entry":
-    # Getting an uploaded file
-    entry_file = request.FILES['efile']
-    fs = FileSystemStorage()
-    filename = fs.save(entry_file.name, entry_file)
-    # Getting file URL
-    file_url = fs.url(filename)
-    debug_field += "Filename: {}\n".format(filename)
-    debug_field += "File url: {}\n".format(file_url)
-    # Getting an entry data
-    ename = request.POST.get("ename", "None")
-    ecat_pk = int(request.POST.get("ecat", 0))
-    ecat = GalleryEntryCategory.objects.get(id=ecat_pk)
-    etype_pk = int(request.POST.get("etype", 0))
-    etype = GalleryEntryType.objects.get(id=etype_pk)
-    edesc = request.POST.get("edesc", "")
-    efdesc = request.POST.get("efdesc", "")
-    # Creating a new entry
-    editor_add_new_entry(ename, etype, ecat, filename, edesc, efdesc)
+    try:
+      # Getting an uploaded file
+      entry_file = request.FILES['efile']
+      fs = FileSystemStorage()
+      filename = fs.save(entry_file.name, entry_file)
+      # Getting file URL
+      file_url = fs.url(filename)
+      # Getting an entry data
+      ename = request.POST.get("ename", "None")
+      ecat_pk = int(request.POST.get("ecat", 0))
+      ecat = GalleryEntryCategory.objects.get(id=ecat_pk)
+      etype_pk = int(request.POST.get("etype", 0))
+      etype = GalleryEntryType.objects.get(id=etype_pk)
+      edesc = request.POST.get("edesc", "")
+      efdesc = request.POST.get("efdesc", "")
+      # Creating a new entry
+      editor_add_new_entry(ename, etype, ecat, filename, edesc, efdesc)
 
-    show_success = True
-    success_msg = "Entry '{}' added to category '{}' successfuly.\n".format(ename, ecat.category_name)
+      show_success = True
+      success_msg = "Entry '{}' added to category '{}' successfuly.\n".format(ename, ecat.category_name)
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
+
 
   # Edit category name
   if editor_type == "edit_cat":
-    cat_pk = int(request.POST.get("cat", 0))
-    cat = GalleryEntryCategory.objects.get(id=cat_pk)
-    orig_cat_name = cat.category_name
-    cat.category_name = request.POST.get("cat-name", "None")
-    cat.save()
+    try:
+      # getting category pk
+      cat_pk = int(request.POST.get("cat", 0))
+      cat = GalleryEntryCategory.objects.get(id=cat_pk)
+      # editing category name and saving it
+      orig_cat_name = cat.category_name
+      cat.category_name = request.POST.get("cat-name", "None")
+      cat.save()
     
-    show_success = True
-    success_msg = "Changed '{}' to '{}'.".format(orig_cat_name, cat.category_name)
+      show_success = True
+      success_msg = "Changed '{}' to '{}'.".format(orig_cat_name, cat.category_name)
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
 
 
   # Edit entry
@@ -203,78 +218,146 @@ def editor(request):
   # Then the entry is edited
   # Selecting category
   if editor_type == "edit_entry_select_cat":
-    selected_cat_pk = int(request.POST.get("cat", 0))
-    selected_cat = GalleryEntryCategory.objects.get(id=selected_cat_pk)
-    mode = request.POST.get("mode", "edit")
-    c = { 
-      "entries": GalleryEntry.objects.filter(entry_category=selected_cat), 
-      "mode": mode,
-    }
-    return render(request, 'home/gallery_editor_select_entry.html', context=c)
+    try:
+      # Getting selected category pk, to find an entry in it
+      selected_cat_pk = int(request.POST.get("cat", 0))
+      if selected_cat_pk >= 0:
+        # getting the category
+        selected_cat = GalleryEntryCategory.objects.get(id=selected_cat_pk)
+        # getting edit mode
+        mode = request.POST.get("mode", "edit")
+        c = { 
+          # Getting all the entries of a category
+          "entries": GalleryEntry.objects.filter(entry_category=selected_cat), 
+          "mode": mode,
+        }
+        return render(request, 'home/gallery_editor_select_entry.html', context=c)
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
 
   # Selecting entry to edit 
   if editor_type == "edit_entry_select_entry":
-    selected_entry_pk = int(request.POST.get("entry_pk",0))
-    selected_entry = GalleryEntry.objects.get(id=selected_entry_pk)
-    mode = request.POST.get("mode", "edit")
-
-    # get data for context
-    context = {
-      "mode": mode,
-      "epk": selected_entry_pk,
-      "ename": selected_entry.entry_name,
-      "ecat": selected_entry.entry_category.pk,
-      "edesc": selected_entry.entry_desc,
-      "efdesc": selected_entry.entry_desc_full,
-      "categories": GalleryEntryCategory.objects.all(),
-    }
-    return render(request, 'home/gallery_editor_edit_entry.html', context=context)
+    try:
+      # getting selected entry pk
+      selected_entry_pk = int(request.POST.get("entry_pk",0))
+      selected_entry = GalleryEntry.objects.get(id=selected_entry_pk)
+      # getting edit mode
+      mode = request.POST.get("mode", "edit")
+      # get data for context
+      context = {
+        "mode": mode,
+        "epk": selected_entry_pk,
+        "ename": selected_entry.entry_name,
+        "ecat": selected_entry.entry_category.pk,
+        "edesc": selected_entry.entry_desc,
+        "efdesc": selected_entry.entry_desc_full,
+        "categories": GalleryEntryCategory.objects.all(),
+      }
+      # render edit or delete page depending on context.mode
+      return render(request, 'home/gallery_editor_edit_entry.html', context=context)
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
 
   
+  # Saving entry edit
   if editor_type == "edit_entry_save_entry":
-    entry_pk = request.POST.get("epk")
-    new_entry_name = request.POST.get("ename")
-    new_entry_cat_pk = int(request.POST.get("ecat"))
-    new_entry_cat = GalleryEntryCategory.objects.get(id=new_entry_cat_pk)
-    new_entry_desc = request.POST.get("edesc")
-    new_entry_desc_full = request.POST.get("efdesc")
+    try:
+      # getting original entry pk
+      entry_pk = request.POST.get("epk")
+      # getting new entry values
+      new_entry_name = request.POST.get("ename")
+      new_entry_cat_pk = int(request.POST.get("ecat"))
+      new_entry_cat = GalleryEntryCategory.objects.get(id=new_entry_cat_pk)
+      new_entry_desc = request.POST.get("edesc")
+      new_entry_desc_full = request.POST.get("efdesc")
+      # overwriting old values with new ones
+      entry = GalleryEntry.objects.get(id=entry_pk)
+      entry.entry_name = new_entry_name
+      entry.entry_category = new_entry_cat
+      entry.entry_desc = new_entry_desc
+      entry.entry_desc_full = new_entry_desc_full
+      # saving an entry
+      entry.save()
 
-    entry = GalleryEntry.objects.get(id=entry_pk)
-    entry.entry_name = new_entry_name
-    entry.entry_category = new_entry_cat
-    entry.entry_desc = new_entry_desc
-    entry.entry_desc_full = new_entry_desc_full
-    entry.save()
-
-    show_success = True
-    success_msg = "Entry edited successfuly."
+      show_success = True
+      success_msg = "Entry edited successfuly."
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
 
 
+  # Deleting entry
   if editor_type == "edit_entry_delete_entry":
-    entry_pk = request.POST.get("epk")
-    entry = GalleryEntry.objects.get(id=entry_pk)
-    entry.delete()
+    try:
+      # Getting entry pk and deleting it
+      entry_pk = request.POST.get("epk")
+      entry = GalleryEntry.objects.get(id=entry_pk)
+      entry.delete()
     
-    show_success = True
-    success_msg = "Entry deleted successfuly."
+      show_success = True
+      success_msg = "Entry deleted successfuly."
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
 
+
+  # Cancel something.
   if editor_type == "cancel":
     pass
 
+
+  # Delete category and all of its content
+  if editor_type == "edit_entry_delete_cat":
+    try:
+      # Getting category pk
+      selected_cat_pk = int(request.POST.get("cat", 0))
+      if selected_cat_pk >= 0:
+        selected_cat = GalleryEntryCategory.objects.get(id=selected_cat_pk)
+        category_name = selected_cat.category_name
+        category_pk = selected_cat.pk
+        c = {
+          "cname": category_name,
+          "cpk": category_pk,
+        }
+        # Rendering category delete confirmation page
+        return render(request, "home/gallery_editor_confirm_delete_cat.html", context=c)
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
+
+
+  # Confirming category delete
+  if editor_type == "edit_confirm_delete_cat":
+    try:
+      # Getting category and all entries in it
+      selected_cat_pk = int(request.POST.get("cpk", 0))
+      selected_cat = GalleryEntryCategory.objects.get(id=selected_cat_pk)
+      entries = GalleryEntry.objects.filter(entry_category=selected_cat),
+      # Deleting category and entries
+      selected_cat.delete()
+      for e in entries:
+        e.delete()
+      
+      show_success = True
+      success_msg = "Category deleted successfuly."
+    except Exception as e:
+      show_error = True
+      error_msg = "ERROR!"
+
+  # Render context
   context = {
     "show_debug": show_debug,
     "debug_field": debug_field,
-
     "show_success_msg": show_success,
     "success_msg": success_msg,
-
     "show_error": show_error,
     "error_msg": error_msg,
-
     "categories": GalleryEntryCategory.objects.all(),
     "entry_types": GalleryEntryType.objects.all(),
   }
-
+  # Rendering editor html
   return render(request, 'home/gallery_editor.html', context=context)
 
 
